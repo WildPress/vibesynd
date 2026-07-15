35d7f6b229fbb44ed8c685dfa298b332271afe27
James Whayman <james@wildpress.co>
2026-07-15 18:38:01 +0100

    lib: upgrade 0x39158 db->clean C (boolean predicate wrapper) -- 7 db upgrades
    return FUN_00039bd7(g_11e2c, g_11e0c[g_bbec]) == 1 (16-bit bool via unsigned short return,
    sete al; xor ah,ah). -4s recipe. 0x39280 skipped (register-arg convention).

--- src/lib/runtime/FUN_00039158.c
+++ src/lib/runtime/FUN_00039158.c
@@ -1,8 +1,9 @@
-/* @ 0x39158 (41B) -- db-transcription (hand-asm/library). */
-
-extern void __db_FUN_00039158_0(void);
-#pragma aux __db_FUN_00039158_0 = "db 83" "db 15" "db 190" "db 5" "db 236" "db 187" "db 0" "db 0" "db 139" "db 20" "db 133" "db 12" "db 30" "db 1" "db 0" "db 82" "db 139" "db 29" "db 44" "db 30" "db 1" "db 0" "db 83" "db 232" "db 99" "db 10" "db 0" "db 0" "db 131" "db 196" "db 8" "db 131" "db 248" "db 1" "db 15" "db 148" "db 192" "db 48" "db 228" "db 91" modify exact [eax ebx ecx edx esi edi ebp];
-#pragma aux FUN_00039158 modify [eax ebx ecx edx esi edi ebp];
-void FUN_00039158(void) {
-    __db_FUN_00039158_0();
+/* @ 0x39158 -- returns (FUN_00039bd7(g_11e2c, g_11e0c[g_bbec]) == 1). */
+extern signed char g_bbec;
+extern int g_11e0c[];
+extern int g_11e2c;
+extern int FUN_00039bd7(int a, int b);
+unsigned short FUN_00039158(void)
+{
+    return FUN_00039bd7(g_11e2c, g_11e0c[g_bbec]) == 1;
 }
