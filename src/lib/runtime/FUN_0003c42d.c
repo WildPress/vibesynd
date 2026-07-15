5e9bbc0b0a3737a4618e8f2494803fc3784566cf
James Whayman <james@wildpress.co>
2026-07-15 19:31:13 +0100

    lib: upgrade 0x3c42d strupr db->clean C (-3s -d2 -os) -- 7 db upgrades
    Cracked via the -3s -d2 push-ebx-first prologue + single-param signature (arg1 at
    [ebp+0xc]) + -os for the top-tested loop form (-oneatx rotates to do-while). Clean while
    loop over toupper (0x3dce5). Confirms -3s -d2 -os as the framed-ebx-with-loop CLIB recipe.

--- src/lib/runtime/FUN_0003c42d.c
+++ src/lib/runtime/FUN_0003c42d.c
@@ -1,8 +1,9 @@
-/* @ 0x3c42d (32B) -- db-transcription (hand-asm/library). */
-
-extern void __db_FUN_0003c42d_0(void);
-#pragma aux __db_FUN_0003c42d_0 = "db 83" "db 85" "db 137" "db 229" "db 139" "db 93" "db 12" "db 128" "db 59" "db 0" "db 116" "db 17" "db 15" "db 182" "db 3" "db 80" "db 232" "db 163" "db 24" "db 0" "db 0" "db 131" "db 196" "db 4" "db 136" "db 3" "db 67" "db 235" "db 234" "db 93" "db 91" modify exact [eax ebx ecx edx esi edi ebp];
-#pragma aux FUN_0003c42d modify [eax ebx ecx edx esi edi ebp];
-void FUN_0003c42d(void) {
-    __db_FUN_0003c42d_0();
+/* @ 0x3c42d -- strupr: uppercase each char of s via toupper (0x3dce5). Recipe: -3s -d2 -os. */
+extern int FUN_0003dce5(int c);
+void FUN_0003c42d(char *s)
+{
+    while (*s != 0) {
+        *s = (char)FUN_0003dce5((unsigned char)*s);
+        s++;
+    }
 }
